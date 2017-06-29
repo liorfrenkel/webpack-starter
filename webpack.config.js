209bb4b991d54ab41b91ce9e4e5e112c38e8a7b3
Lior Frenkel <hotfur@gmail.com>
2017-06-29 12:30:37 +0200

    Add hash to output file

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, './dist/'),
-    filename: '[name].bundle.js',
+    filename: '[name].bundle.[hash].js',
   },
   plugins: [
     new webpack.optimize.ModuleConcatenationPlugin(),
